3bcb54a2c342de62f28bf507adcf8e749b0fc28a
rmwdeveloper <rmwdeveloper@gmail.com>
2016-02-21 16:54:09 -0500

    Initial commit

---
+++ site/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var fortunes = require('../lib/fortune.js');
+var app = express();
+
+
+var handlebars = require('express3-handlebars')
+	.create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+			req.query.test == '1';
+	next();
+});
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	res.render('about', { fortune: fortunes.getFortune(),
+						pageTestScript: '/qa/tests-about.js'});
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err,req, res, next){
+	console.log(err.stack);
+	res.status(500);
+	res.render('500');
+});
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate.' );
+});
+
